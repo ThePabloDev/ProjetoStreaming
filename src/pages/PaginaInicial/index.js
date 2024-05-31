b29eb6a598b547fb9e6eb998fe531602934a7df4
Pablo Gabriel <pabloglara@outlook.com>
2024-05-30 23:09:48 -0300

    mexi um pouco na page inicial

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -1,22 +1,41 @@
 import { Link } from 'react-router-dom';
-import './PaginaInicial.css'
-import imagemcapa from './img/capa.png'
+import './PaginaInicial.css';
+import imagemcapa from './img/capa.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import imgpipoca from './img/pipoca.png'
 
 function PaginaInicial() {
     return (
-        <div className='fonte'>
-            <div className='foto-container'>
-                <img src={imagemcapa} className='foto-capa' />
-                <img src={imagemcapa} className='foto-capa' />
+        <body className="pagina-inicial">
+            <Navbar fixed="top">
+                <Container>
+                    <Navbar.Brand href="./">
+                        <strong className='texto-nav'>FLICKER</strong>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Link to="/login">
+                            <button className='BotaoLogin'><strong>ENTRAR</strong></button>
+                        </Link>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className='fonte'>
+                <div className='foto-container'>
+                    <img src={imagemcapa} className='foto-capa' alt="Capa" />
+                </div>
             </div>
-            <div className='texto-foto'>
-                <h1>NoSignal</h1>
+            <div className='container-texto'>
+                <p className='texto-titulo'><strong>Filmes, Séries, músicas e streamings</strong></p>
+                <p className='texto-page'>Agora você pode assistir seus filmes e séries favoritos onde e quando quiser!
+                    Seja no conforto do seu sofá, durante a viagem ou até mesmo no intervalo do trabalho,
+                    a diversão está sempre ao seu alcance.
+                </p>
+                <button className='BotaoCadastro BotaoCadastroPosition'><strong>Vamos lá</strong></button>
             </div>
-            <div className='BotaoLoginPosicao'>
-                <button className='BotaoLogin'><strong>ENTRAR</strong></button>
-            </div>
-        </div>
-
+        </body>
     );
 }
 
