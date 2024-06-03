cd563be9d1856475d93c4e630fd12039684a5d3a
Pablo Gabriel <pabloglara@outlook.com>
2024-06-03 11:18:52 -0300

    pagina inicial quase finalizada, faltando media queries para tablet, laptop e desktop

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -8,7 +8,7 @@ import imgdispositivo from './img/dispositivo.png';
 
 function PaginaInicial() {
     return (
-        <body className="pagina-inicial">
+        <body className="pagina-inicial container">
             <Navbar fixed="top">
                 <Container>
                     <Navbar.Brand href="./">
@@ -22,18 +22,18 @@ function PaginaInicial() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <div className='fonte'>
+            <div>
                 <div className='foto-container'>
                     <img src={imagemcapa} className='foto-capa' alt="Capa" />
                 </div>
             </div>
-            <div className='container-texto'>
+            <div className='container-principal '>
                 <p className='texto-titulo'><strong>Filmes, Séries, músicas e streamings</strong></p>
                 <p className='texto-page'>Agora você pode assistir seus filmes e séries favoritos onde e quando quiser!
                     Seja no conforto do seu sofá, durante a viagem ou até mesmo no intervalo do trabalho,
                     a diversão está sempre ao seu alcance.
                 </p>
-                <button className='BotaoCadastro BotaoCadastroPosition'><strong>Vamos lá</strong></button>
+                <button className='BotaoCadastro BotaoCadastroPosition '><strong>Vamos lá</strong></button>
                 <div className="card">
                     <div className="card-content">
                         <p className="card-title">FLICKER</p>
@@ -41,8 +41,8 @@ function PaginaInicial() {
                         <button className="botao-card"><strong>PLANOS</strong></button>
                     </div>
                 </div>
-                <div>
-                    <img src={imgdispositivo} alt='imagem de varios dispositivos' className='img-dispositivo' />
+                <div className='alinhar'>
+                    <img src={imgdispositivo} alt='imagem de varios dispositivos' className='img-dispositivo alinhar' />
                 </div>
                 <div>
                     <br />
@@ -51,7 +51,49 @@ function PaginaInicial() {
                         aproveite a flexibilidade de acompanhar suas séries, filmes e vídeos onde quer que esteja. A qualquer hora, em qualquer lugar,
                         a diversão está garantida. Basta conectar e aproveitar!</p>
                 </div>
+                <br />
+                <section id="card1" className="card alinhar">
+                    <div class="question">
+                        <p>Qual é o principal tipo de conteúdo que o site Flicker oferece aos seus usuários?</p>
+                    </div>
+                    <div class="card__content">
+                        <p class="card__description">
+                            O site Flicker oferece principalmente filmes, séries, músicas e streamings,
+                            permitindo que os usuários assistam aos seus conteúdos favoritos em qualquer lugar e a qualquer momento.
+                        </p>
+                    </div>
+                </section>
+                <br />
+                <section id="card1" class="card">
+                    <div class="question">
+                        <p>Que informações o site Flicker fornece sobre os planos de assinatura disponíveis?</p>
+                    </div>
+                    <div class="card__content">
+                        <p class="card__description">
+                            O site Flicker oferece diferentes planos de assinatura que atendem a diversas necessidades,
+                            desde soluções básicas até opções premium, incentivando os usuários a escolher o plano que melhor se adequa às suas necessidades.
+                        </p>
+                    </div>
+                </section>
+                <br />
+                <section id="card1" class="card">
+                    <div class="question">
+                        <p>Como o site Flicker garante a flexibilidade de assistir conteúdo em diferentes dispositivos?</p>
+                    </div>
+                    <div class="card__content">
+                        <p class="card__description">
+                            O site Flicker permite que os usuários assistam a conteúdos em diversos dispositivos, como celular, tablet,
+                            computador e Smart TV, garantindo que possam aproveitar seus filmes e séries favoritos onde quer que estejam.
+                        </p>
+                    </div>
+                </section>
+                <br />
+                <br />
+                <footer>
+                    <a><strong className='footer'>FLICKER</strong></a>
+                </footer>
             </div>
+
         </body>
     );
 }
